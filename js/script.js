1bfad0092c8ba2053879abf9e7d83a5d6643a5ca
sharon-ku <sharon-ku@users.noreply.github.com>
2021-04-08 23:00:28 -0400

    CP: Deleted all previous code using JSON file

--- js/script.js
+++ js/script.js
@@ -14,9 +14,6 @@ Code for color picker from: https://www.youtube.com/watch?v=8fJ2xGq5e7s&ab_chann
 // Count turn we are at
 let currentTurn = 0;
 
-// Contains JSON file
-let rectangle;
-
 // Stores RGBA value of color picker
 let colorValue = undefined;
 
@@ -82,9 +79,6 @@ let artwork = {
 //
 // Preload assets
 function preload() {
-  // Load JSON file
-  rectangle = loadJSON(`assets/data/rectangles.json`);
-
   // Load artwork image
   artwork.image = loadImage(`assets/images/artwork.jpg`);
 }
@@ -101,14 +95,8 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  // Set bg color
   background(0);
-
-  // Display black area for artwork
-  // push();
-  // fill(artwork.fill);
-  // // artwork.y = height / 2 - artwork.height / 2;
-  // rect(artwork.x, artwork.y, artwork.width, artwork.height);
-  // pop();
 
   // Display artwork image
   push();
@@ -136,30 +124,6 @@ function draw() {
 
   // Drop box 2
   drawDropBox(dropBox2);
-
-  // let locationIndex = 0;
-  // // Property from specific rectangle in JSON file
-  // for (
-  //   let i = 0;
-  //   i < rectangle.rectangleProperties[locationIndex].rectangles.length;
-  //   i++
-  // ) {
-  //   let rectangleProperties =
-  //     rectangle.rectangleProperties[locationIndex].rectangles[i];
-  //
-  //   displayRect(rectangleProperties);
-  //
-  //   if (checkOverlap(rectangleProperties)) {
-  //     // If current turn is an even number, update dropBox1 color
-  //     if (currentTurn % 2 == 0) {
-  //       dropBox1.fill = rectangleProperties.fill;
-  //     }
-  //     // Or else, if odd number, update dropBox2 color
-  //     else {
-  //       dropBox2.fill = rectangleProperties.fill;
-  //     }
-  //   }
-  // }
 }
 
 // Draw dropboxes
@@ -171,41 +135,6 @@ function drawDropBox(dropBox) {
   fill(dropBox.fill.r, dropBox.fill.g, dropBox.fill.b);
   rect(dropBox.x, dropBox.y, dropBox.width, dropBox.height);
   pop();
-}
-
-// Display a rectangle using given arguments
-function displayRect(rectangle) {
-  push();
-  rectMode(CENTER);
-  fill(rectangle.fill.r, rectangle.fill.g, rectangle.fill.b);
-  rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-  pop();
-}
-
-function mouseDragged() {
-  // // If mouse overlaps with rectangle, drag rectangle
-  // if (
-  //   mouseX > rect1.x - rect1.size / 2 &&
-  //   mouseX < rect1.x + rect1.size / 2 &&
-  //   mouseY > rect1.y - rect1.size / 2 &&
-  //   mouseY < rect1.y + rect1.size / 2
-  // ) {
-  //   rect1.x = mouseX;
-  //   rect1.y = mouseY;
-  // }
-}
-
-function checkOverlap(rectangle) {
-  if (
-    mouseX > rectangle.x - rectangle.width / 2 &&
-    mouseX < rectangle.x + rectangle.width / 2 &&
-    mouseY > rectangle.y - rectangle.height / 2 &&
-    mouseY < rectangle.y + rectangle.height / 2
-  ) {
-    return true;
-  } else {
-    return false;
-  }
 }
 
 function mouseReleased() {
@@ -226,17 +155,4 @@ function mouseReleased() {
 
   // Add 1 to number of turns
   currentTurn++;
-
-  // if (
-  //   rect1.x < dropBox1.x + dropBox1.width / 2 &&
-  //   rect1.x > dropBox1.x - dropBox1.width / 2 &&
-  //   rect1.y < dropBox1.y + dropBox1.width / 2 &&
-  //   rect1.y > dropBox1.x - dropBox1.width / 2
-  // ) {
-  //   dropBox1.fill = rect1.fill;
-  // }
 }
-
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-// }
